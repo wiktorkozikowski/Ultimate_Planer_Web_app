a745d0e70284ade59dafc05f2f3b972199696924
Wiktor Kozikowski <wiktorkozikowski.main@gmail.com>
2026-04-01 14:19:16 +0200

    fix docker front volumes

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 
 export default function App() {
   return (
-    <main className="app">
+    <main className="app" data-testid="app-root">
       <h1>Cześć React 👋</h1>
-      <p>To jest moja pierwsza strona startowa.</p>
+      <p>To jest pierwszy element.</p>
+      <p>To jest drugi element.</p>
+      <button>Ttest</button>
     </main>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './styles/index.css';
 
-const element = React.createElement('div');
-
-console.log(element);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
